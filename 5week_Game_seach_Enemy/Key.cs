31782cba2b12a02a25462e47ad30cdb19b7afd4f
YUHAN <YUHAN@PC7207_18>
2023-04-13 10:57:28 +0900

    0413

--- 5week_Game_seach_Enemy/Key.cs
+++ 5week_Game_seach_Enemy/Key.cs
@@ -5,14 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace _5week_Game_seach_Enemy
-{
-    internal class Key
-    {
-        //상하좌우 엔터
-
-
-
-    }
+{    
      class Input_Key
     {
          public string Input_KeyRead()
